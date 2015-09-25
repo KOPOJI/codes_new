7f9cdc86527136c48ed1710ce78494825f716464
KOPOJI <kopojiq@mail.ru>
2015-09-26 00:25:44 +0300

    first commit

--- translations/lupdate_lrelease.sh
+++ translations/lupdate_lrelease.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-lupdate `find ./ \( -name '*.cpp' -o -name '*.h' -o -name '*.erb' \) -not \( -path './views/_src/*' -o -name '*moc*' \)` -locations absolute -ts ./en.ts -ts ./ru.ts
+lupdate `find . \( -name '*.cpp' -o -name '*.h' -o -name '*.erb' \) -not \( -path './views/_src/*' -o -name '*moc*' \)` -locations absolute -ts ./translations/en.ts -ts ./translations/ru.ts
 lrelease `find ./translations -name '*.ts'`
